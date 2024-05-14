a56409c678804343cc6c7cb50a872be8067ed3a5
Alexey Chernov <alexchernov063@gmail.com>
2024-05-14 12:10:38 +0200

    1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,11 +2,10 @@
 
 const fs = require('fs');
 const filePath = process.argv[2];
+const content = process.argv[3];
 
-fs.readFile(filePath, 'utf-8', (error, content) => {
+fs.writeFile(filePath, content, 'utf-8', (error) => {
   if (error) {
     console.log(error);
-  } else {
-    console.log(content);
   }
 });
